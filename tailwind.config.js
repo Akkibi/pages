b93795e8be391cd99002ad5b525530fe81a904bb
Akkibi <akiravalade@gmail.com>
2023-01-07 20:30:43 +0100

    ajout du site local sur git

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  // purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, //or 'media' or 'class'
+  content: ["./build/**/*.{html,js}"],
+  theme: {
+    extend: {
+      // keyframes: {
+      //   movy: {
+      //     "0%": { transform: "translateY(0%)" },
+      //     "100%": { transform: "translateY(50%)" },
+      //   },
+      //   movx: {
+      //     "0%": { transform: "translateX(0%)" },
+      //     "100%": { transform: "translateX(50%)" },
+      //   },
+      // },
+      // animation: {
+      //   slidex: "movx 10s infinite",
+      //   slidey: "movy 10s infinite",
+      // },
+      colors: {
+        blue: {
+          1000: "#001382",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
