83a191d80ac8448f853c2f94e896501c8e587852
Akkibi <88116996+Akkibi@users.noreply.github.com>
2023-03-02 01:53:48 +0100

    add error page and not load element when can't

--- src/dataload.js
+++ src/dataload.js
@@ -33,79 +33,73 @@ function updateContent() {
       const projectData = data[category][id];
       if (!projectData) {
         console.error("Project not found in data.json");
-      }
-      console.log(
-        document.querySelector(".backgroundReplace").style.background
-      );
+      } else {
+        //replace Date, Title and description
+        document.getElementById("dateReplace").innerText = projectData.date;
+        document.getElementById("titleReplace").innerText = projectData.title;
+        document.getElementById("descriptionReplace").innerText =
+          projectData.description;
 
-      //replace Date, Title and description
-      document.getElementById("dateReplace").innerText = projectData.date;
-      document.getElementById("titleReplace").innerText = projectData.title;
-      document.getElementById("descriptionReplace").innerText =
-        projectData.description;
+        mainImage = document.querySelector(".backgroundReplace").style;
+        mainImage.background =
+          "url(../assets/" +
+          projectData.name +
+          "/" +
+          projectData.images[0] +
+          ") center";
+        mainImage.backgroundSize = "cover";
 
-      mainImage = document.querySelector(".backgroundReplace").style;
-      mainImage.background =
-        "url(../assets/" +
-        projectData.name +
-        "/" +
-        projectData.images[0] +
-        ") center";
-      mainImage.backgroundSize = "cover";
-
-      console.log(
-        document.querySelector(".backgroundReplace").style.background
-      );
-      if (projectData.videos.length >= 1) {
-        for (j = 0; j < projectData.videos.length; j++) {
-          console.log("video", j, projectData.name, projectData.videos[j]);
-          document.querySelector(".cardsInsert").innerHTML +=
-            `
+        if (projectData.videos.length >= 1) {
+          for (j = 0; j < projectData.videos.length; j++) {
+            console.log("video", j, projectData.name, projectData.videos[j]);
+            document.querySelector(".cardsInsert").innerHTML +=
+              `
           <button
           onclick="openCurrentVideo('` +
-            projectData.videos[j] +
-            `')"
+              projectData.videos[j] +
+              `')"
           class="mr-[2.5vw] aspect-[2/3] rounded-3xl border-2 border-blue-first"
           style="
             background: url(../assets/` +
-            projectData.name +
-            `/` +
-            projectData.videos[j] +
-            `) center;
+              projectData.name +
+              `/` +
+              projectData.videos[j] +
+              `) center;
             background-size: cover;
           "
         >
         <div class="h-full w-full" style="background:url(../public/play.png) no-repeat center;  background-size: 5rem 5rem;"></div>
         </button>`;
+          }
         }
-      }
-      if (projectData.images.length > 1) {
-        for (i = 1; i < projectData.images.length; i++) {
-          console.log("image", i, projectData.name, projectData.images[i]);
-          document.querySelector(".cardsInsert").innerHTML +=
-            `
+        if (projectData.images.length > 1) {
+          for (i = 1; i < projectData.images.length; i++) {
+            console.log("image", i, projectData.name, projectData.images[i]);
+            document.querySelector(".cardsInsert").innerHTML +=
+              `
           <button
           onclick="openCurrentImage('` +
-            projectData.images[i] +
-            `')"
+              projectData.images[i] +
+              `')"
           class="mr-[2.5vw] aspect-[2/3] rounded-3xl border-2 border-blue-first"
           style="
             background: url(../assets/` +
-            projectData.name +
-            `/` +
-            projectData.images[i] +
-            `) center;
+              projectData.name +
+              `/` +
+              projectData.images[i] +
+              `) center;
             background-size: cover;
           "
         ></button>`;
+          }
         }
-      }
 
-      //update opacity on load
-      if (id + 1 <= data[category].length) {
-        updateOpacity(data[category].length);
-      } else {
-        updateOpacity();
+        //update opacity on load
+        if (id + 1 <= data[category].length) {
+          updateOpacity(data[category].length);
+        } else {
+          updateOpacity();
+        }
       }
     });
 }
@@ -121,7 +115,7 @@ function openCurrentVideo(nomVideo) {
       if (!projectData) {
         console.error("Project not found in data.json");
       }
-      document.getElementById("VideoElement").style.display = "block";
+      document.getElementById("videoElement").style.display = "block";
       document.getElementById("loadElement").style.display = "block";
       urlVideo = nomVideo.substr(0, nomVideo.length - 4);
       document.getElementById("youtubeIframe").innerHTML =
@@ -137,10 +131,6 @@ function openCurrentVideo(nomVideo) {
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           allowfullscreen>
         </iframe>`;
-      console.log(
-        document.getElementById("videoElement").innerHTML,
-        document.getElementById("loadElement").innerHTML
-      );
     });
 }
 
@@ -151,42 +141,69 @@ function openCurrentImage(nomImage) {
       // Get the project data from data.json based on the category and id
       const projectData = data[category][id];
       if (!projectData) {
-        console.error("Project not found in data.json");
-      }
-      bigImage = document.getElementById("imageElement").style;
-      if (!nomImage) {
-        bigImage.background =
-          "url(../assets/" +
-          projectData.name +
-          "/big-" +
-          projectData.images[0] +
-          ") no-repeat center";
+        console.error(
+          "Sorry but this picture is unavailable. Project not found in data.json"
+        );
       } else {
-        bigImage.background =
-          "url(../assets/" +
-          projectData.name +
-          "/big-" +
-          nomImage +
-          ") no-repeat center";
+        bigImage = document.getElementById("imageElement").style;
+        // console.log(
+        //   "bahahaha",
+        //   imageExists(
+        //     "../assets/" + projectData.name + "/big-" + projectData.images[0]
+        //   )
+        // );
+        if (!nomImage) {
+          if (
+            imageExists(
+              "../assets/" + projectData.name + "/big-" + projectData.images[0]
+            )
+          ) {
+            //affiche la bonne image
+            bigImage.background =
+              "url(../assets/" +
+              projectData.name +
+              "/big-" +
+              projectData.images[0] +
+              ") no-repeat center";
+          } else {
+            //affiche 404
+            bigImage.background =
+              "url(../public/error404.png) no-repeat center";
+          }
+        } else {
+          //affiche la bonne image
+          if (
+            imageExists("../assets/" + projectData.name + "/big-" + nomImage)
+          ) {
+            bigImage.background =
+              "url(../assets/" +
+              projectData.name +
+              "/big-" +
+              nomImage +
+              ") no-repeat center";
+          } else {
+            //affiche 404
+            bigImage.background =
+              "url(../public/error404.png) no-repeat center";
+          }
+        }
+        document.getElementById("loadElement").style.display = "block";
+        document.getElementById("imageElement").style.display = "block";
+        bigImage.backgroundSize = "contain";
       }
-      document.getElementById("loadElement").style.display = "block";
-      document.getElementById("imageElement").style.display = "block";
-      bigImage.backgroundSize = "contain";
     });
 }
 function closeBig() {
-  console.log("close image");
   loadElement = document.getElementById("loadElement");
   imageElement = document.getElementById("imageElement");
-  if (document.getElementById("my-video") != undefined) {
-    currentVideo = document.getElementById("my-video");
-    currentVideo.currentTime = 10000;
-    currentVideo.remove();
-    bigImage.innerHTML = "";
-  }
+  videoElement = document.getElementById("videoElement");
+  youtubeIframe = document.getElementById("youtubeIframe");
+
+  loadElement.style.display = null;
   imageElement.style.display = null;
   imageElement.style.backgroundImage = null;
-  loadElement.style.display = null;
+  videoElement.style.display = null;
+  youtubeIframe.innerHTML = "";
 }
 
 //when right arrow is clicked
@@ -264,8 +281,11 @@ scrollablePhone.addEventListener("wheel", (event) => {
   scrollablePhone.scrollLeft -= delta * 30;
 });
 
-// document.getElementById("my-video").addEventListener("click", (event) => {
-//   document.getElementById("my-video");
-//   // event.preventDefault();
-//   console.log("arret!!", event);
-// });
+function imageExists(image_url) {
+  var http = new XMLHttpRequest();
+
+  http.open("HEAD", image_url, false);
+  http.send();
+
+  return http.status != 404;
+}
